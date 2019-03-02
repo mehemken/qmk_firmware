0b3783a05db8a39ac9698605e685c1d72d2021d7
Marco Hemken <mehemken@gmail.com>
2019-03-02 11:57:46 -0800

    better home,end,insert buttons

--- keyboards/preonic/keymaps/mehemken/keymap.c
+++ keyboards/preonic/keymaps/mehemken/keymap.c
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * | Caps |      |      |      |      |      |      |      | Home | End  | Ins  | Mute |
+ * | Caps |      |      |      |      |      |      |      |      | Home | End  | Ins  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |   \  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
@@ -142,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid( \
-  KC_CAPS, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  KC_INS,  KC_MUTE,  \
+  KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  KC_INS,  \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,  \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_ENTER, \
   _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PGDN, KC_PGUP, KC_BRID, KC_BRIU,  _______, \
